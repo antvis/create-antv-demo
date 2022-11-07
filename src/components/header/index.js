1ec1d1a798b28ca6a24b20da037f820d97e001c0
neoddish <neoddish@outlook.com>
2022-11-08 00:27:21 +0800

    feat: monkey king title

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,7 @@ import styles from './style.module.scss';
 export const Header = () => {
   return (
     <div className={styles.header}>
-      <h5>AntV大作业</h5>
+      <h5>孙悟空的可视化简历</h5>
     </div>
   );
 };
