a179b37ffbe8bf953d034c8e4ca9815f1afece8f
neoddish <neoddish@outlook.com>
2022-11-08 15:52:47 +0800

    feat: monkey king map

--- src/pages/home/components/map/index.js
+++ src/pages/home/components/map/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, CityBuildingLayer, LineLayer, PolygonLayer } from '@antv/l7';
+import { Scene, LineLayer, PointLayer, CanvasLayer } from '@antv/l7';
 import { GaodeMap } from '@antv/l7-maps';
 import { Card } from 'src/components/card';
+import { mapData } from '../../mock-data';
 
 export const Maps = ({ cardImage }) => {
   const sceneRef = React.useRef();
@@ -19,8 +20,6 @@ export const Maps = ({ cardImage }) => {
 
   const fetchData = React.useCallback(async () => {
     const fetchUrlList = [
-      'https://gw.alipayobjects.com/os/rmsportal/ggFwDClGjjvpSMBIrcEx.json',
-      'https://gw.alipayobjects.com/os/bmw-prod/67130c6c-7f49-4680-915c-54e69730861d.json',
       'https://gw.alipayobjects.com/os/basement_prod/40ef2173-df66-4154-a8c0-785e93a5f18e.json',
     ];
     const res = await Promise.allSettled(
@@ -38,99 +37,82 @@ export const Maps = ({ cardImage }) => {
     };
   }, [fetchData]);
 
-  const initMap = (dataList) => {
-    const [pointLayerData, PolygonLayerData, LineLayerData] = dataList;
+  const initMap = () => {
     const scene = new Scene({
       id: 'map',
       map: new GaodeMap({
-        style: 'amap://styles/2922a1f20fffb827a620c2c5e80f9f4b?isPublic=true',
-        center: [120.145196, 30.239885],
-        pitch: 45,
-        zoom: 13.4,
+        style: 'dark',
+        center: [100, 38],
+        zoom: 4,
       }),
     });
+
     sceneRef.current = scene;
-    const pointLayer = new CityBuildingLayer();
-    pointLayer
-      .source(pointLayerData)
-      .size('floor', [0, 500])
-      .color('rgba(242,246,250,1.0)')
+
+    const dotPoint = new PointLayer({ zIndex: 2 })
+      .source(mapData, {
+        parser: {
+          type: 'json',
+          x: 'lng',
+          y: 'lat',
+        },
+      })
+      .shape('circle')
+      .color('#00FFFF')
+      .animate(true)
+      .size(30);
+    scene.addLayer(dotPoint);
+
+    const lineData = [];
+    for (let i = 0; i < mapData.length - 1; i += 1) {
+      lineData.push({
+        lng1: mapData[i].lng,
+        lat1: mapData[i].lat,
+        lng2: mapData[i + 1].lng,
+        lat2: mapData[i + 1].lat,
+      });
+    }
+
+    const lineLayer = new LineLayer({ blend: 'normal' })
+      .source(lineData, {
+        parser: {
+          type: 'json',
+          x: 'lng1',
+          y: 'lat1',
+          x1: 'lng2',
+          y1: 'lat2',
+        },
+      })
+      .size(1.5)
+      .shape('greatcircle')
       .animate({
         enable: true,
+        interval: 2,
+        trailLength: 1,
+        duration: 5,
       })
-      .active({
-        color: '#0ff',
-        mix: 0.5,
-      })
+      .color('#00FFFF')
       .style({
-        opacity: 0.7,
-        baseColor: 'rgb(16, 16, 16)',
-        windowColor: 'rgb(30, 60, 89)',
-        brightColor: 'rgb(255, 176, 38)',
-        sweep: {
-          enable: true,
-          sweepRadius: 2,
-          sweepColor: '#1990FF',
-          sweepSpeed: 0.5,
-          sweepCenter: [120.145319, 30.238915],
-        },
+        opacity: 0.8,
       });
-    scene.addLayer(pointLayer);
+    scene.addLayer(lineLayer);
 
-    const { lakeBorderData, lakeData, landData } = PolygonLayerData;
-    const lakeLayer = new PolygonLayer()
-      .source(lakeData)
-      .shape('fill')
-      .color('#1E90FF')
-      .style({
-        opacity: 0.4,
-        opacityLinear: {
-          enable: true,
-          dir: 'out', // in - out
-        },
-      });
-    const landLayer = new PolygonLayer()
-      .source(landData)
-      .shape('fill')
-      .color('#3CB371')
-      .style({
-        opacity: 0.4,
-        opacityLinear: {
-          enable: true,
-          dir: 'in', // in - out
-        },
-      });
+    const canvasLayer = new CanvasLayer({}).style({
+      drawingOnCanvas: (option) => {
+        const { size, ctx } = option;
+        const [width, height] = size;
 
-    const lakeBorderLayer = new PolygonLayer()
-      .source(lakeBorderData)
-      .shape('fill')
-      .color('#ccc')
-      .style({
-        opacity: 0.5,
-        opacityLinear: {
-          enable: true,
-          dir: 'in', // in - out
-        },
-      });
+        ctx.clearRect(0, 0, width, height);
+        // canvas 绘制
 
-    scene.addLayer(lakeLayer);
-    scene.addLayer(lakeBorderLayer);
-    scene.addLayer(landLayer);
+        ctx.font = '48px Microsoft YaHei';
+        ctx.fillStyle = 'white';
+        ctx.textAlign = 'center';
 
-    const layer = new LineLayer({
-      zIndex: 0,
-      depth: true,
-    })
-      .source(LineLayerData)
-      .size(1)
-      .shape('line')
-      .color('#1990FF')
-      .animate({
-        interval: 1, // 间隔
-        duration: 2, // 持续时间，延时
-        trailLength: 2, // 流线长度
-      });
-    scene.addLayer(layer);
+        ctx.fillText('西天取经之路', width / 2, height - 80);
+      },
+    });
+    scene.addLayer(canvasLayer);
   };
 
   return (
--- src/pages/home/mock-data.js
+++ src/pages/home/mock-data.js
@@ -36,3 +36,42 @@ export const RadarData = [
   { item: '心性修行', score: 1 },
   { item: '神通', score: 5 },
 ];
+
+export const mapData = [
+  { order: 1, oldName: '长安', todayName: '西安市', lng: 108.94, lat: 34.34 },
+  { order: 2, oldName: '西渭桥', todayName: '咸阳市', lng: 108.71, lat: 34.33 },
+  { order: 3, oldName: '武功县', todayName: '武功县', lng: 108.2, lat: 34.26 },
+  { order: 4, oldName: '岐山县', todayName: '岐山县', lng: 107.62, lat: 34.44 },
+  { order: 5, oldName: '凤翔府', todayName: '凤翔县', lng: 107.4, lat: 34.52 },
+  { order: 6, oldName: '陇州', todayName: '陇县', lng: 106.86, lat: 34.89 },
+  { order: 7, oldName: '上邽县', todayName: '天水市', lng: 105.72, lat: 34.58 },
+  { order: 8, oldName: '达渭州', todayName: '陇西县', lng: 104.63, lat: 35.0 },
+  {
+    order: 9,
+    oldName: '鸟鼠同穴山',
+    todayName: '渭源县',
+    lng: 104.22,
+    lat: 35.14,
+  },
+  {
+    order: 10,
+    oldName: '狄道县',
+    todayName: '临洮县',
+    lng: 103.86,
+    lat: 35.39,
+  },
+  { order: 11, oldName: '兰州', todayName: '兰州', lng: 103.83, lat: 36.06 },
+  { order: 12, oldName: '乌鞘岭', todayName: '乌鞘岭', lng: 102.85, lat: 37.2 },
+  { order: 13, oldName: '凉州', todayName: '武威市', lng: 102.64, lat: 37.93 },
+  { order: 14, oldName: '甘州', todayName: '张掖市', lng: 100.45, lat: 38.92 },
+  { order: 15, oldName: '肃州', todayName: '酒泉市', lng: 98.49, lat: 39.73 },
+  { order: 16, oldName: '瓜州', todayName: '瓜州县', lng: 95.78, lat: 40.52 },
+  { order: 17, oldName: '伊吾国', todayName: '哈密市', lng: 93.52, lat: 42.82 },
+  {
+    order: 17,
+    oldName: '高昌国',
+    todayName: '吐鲁番市东高昌故城',
+    lng: 89.53,
+    lat: 42.85,
+  },
+];
