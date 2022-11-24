89cb76a3ffff6d3a3771cc4f0e755161be9c232e
lxfu1 <954055752@qq.com>
2022-11-24 10:53:24 +0800

    docs: gh pages

--- src/components/header/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './style.module.scss';
-
-export const Header = () => {
-  return (
-    <div className={styles.header}>
-      <h5>孙悟空的可视化简历</h5>
-    </div>
-  );
-};
--- src/constants/index.js
+++
@@ -1,8 +0,0 @@
-export const ThemeColor10 = [
-  '#1677ff',
-  '#39c8ae',
-  '#c781fb',
-  '#f68773',
-  '#feaa4a',
-  '#ed6dc6',
-];
